263c8494711933f0faa38d4904b5b35d37601a1d
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-08-02 20:36:44 +0530

    Update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "@/components/SessionWrapper";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionWrapper>{children}</SessionWrapper>
+        <SessionWrapper>
+          <Suspense>{children}</Suspense>
+        </SessionWrapper>
       </body>
     </html>
   );
